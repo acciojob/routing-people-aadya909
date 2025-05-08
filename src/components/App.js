a3d18210fd8f91095cb6f8a90973a24f999dbf0c
Aadya Sharma <aadya1773sharma@gmail.com>
2025-05-08 17:27:49 +0530

    2

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,20 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import UserList from './UserList';
 import UserDetails from './UserDetails';
 
 function App() {
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<UserList />} />
-                <Route path="/users/:id" element={<UserDetails />} />
-            </Routes>
+            <Switch>
+                <Route exact path="/" component={UserList} />
+                <Route path="/users/:id" component={UserDetails} />
+            </Switch>
         </Router>
     );
 }
 
 export default App;
+
 
